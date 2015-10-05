c1487216a4cd8595e50883567247c83c44015696
pjuftring <uftring@hotmail.de>
2015-10-05 21:58:54 +0200

    Removed seeking of stdin
    On linux, it is not possible to seek stdin, therefore I had to rewrite
    my tokenizer. The good thing is, that it looks better than before.

--- src/Token.c
+++ src/Token.c
@@ -1,63 +1,59 @@
 #include "fractranC.h"
 
 char word[MAX_WORD_LENGTH + 1] = { 0 };
-char* inputBuffer = NULL;
-unsigned long fileSize = 0;
-unsigned long filePosition = 0;
+FILE* inputFile;
+char c = 0;
 
 int isSpace(char c);
+void nextChar();
+
+int lastWasSlash = 0;
+
 
 void initTokenizer(const char* path) {
 	/*FILE* inputFile = fopen(path, "rb");
 	if (inputFile == NULL) {
 		tragicFail("Input file cannot be read.");
 	}*/
-
-	FILE* inputFile = stdin;
-
-	fseek(inputFile, 0, SEEK_END);
-	fileSize = ftell(inputFile);
-	fseek(inputFile, 0, SEEK_SET);
-
-	inputBuffer = malloc(fileSize);
-	if (inputBuffer == NULL) {
-		tragicFail("Memeory can not be allocated.");
-	}
-
-	fread(inputBuffer, 1, fileSize, inputFile);
-
-	fclose(inputFile);
+	
+	inputFile = stdin;
+	nextChar();
 }
 
 int nextWord() {
 	int wordPosition = 0;
 
-	while (isSpace(inputBuffer[filePosition]) && (filePosition < fileSize)) {
-		filePosition++;
+	while (isSpace(c) && (c != EOF)) {
+		nextChar();
 	}
 
-	if (filePosition >= fileSize)
+	if (c == EOF)
 		return 0;
 
-	if ((inputBuffer[filePosition] == '/') && ((filePosition + 1) < fileSize)) {
-		if (inputBuffer[filePosition + 1] == '/') {
-			while (inputBuffer[filePosition] != '\n' && (filePosition < fileSize)) {
-				filePosition++;
+	if (c == '/') {
+		if (lastWasSlash) {
+			while ((c != '\n') && (c != EOF)) {
+				nextChar();
 			}
-			return nextWord();
+		} else {
+			lastWasSlash = 1;
+			nextChar();
 		}
+		return nextWord();
+	} else {
+		lastWasSlash = 0;
 	}
 
-	if ((inputBuffer[filePosition] == '#') || (inputBuffer[filePosition] == ',')) {
-		word[0] = inputBuffer[filePosition];
+	if ((c == '#') || (c == ',')) {
+		word[0] = c;
 		word[1] = 0;
-		filePosition++;
+		nextChar();
 		return 1;
 	}
 
-	while ((!isSpace(inputBuffer[filePosition])) && (filePosition < fileSize) && (wordPosition <= MAX_WORD_LENGTH)) {
-		word[wordPosition] = inputBuffer[filePosition];
-		filePosition++;
+	while ((!isSpace(c)) && (c != EOF) && (wordPosition <= MAX_WORD_LENGTH)) {
+		word[wordPosition] = c;
+		nextChar();
 		wordPosition++;
 	}
 
@@ -66,11 +62,15 @@ int nextWord() {
 }
 
 void killTokenizer() {
-	free(inputBuffer);
+	fclose(inputFile);
 }
 
 // Tokenizer's Little Helper Functions
 
 int isSpace(char c) {
 	return ((c == ' ') || (c == '\t') || (c == '\n') || (c == '\r'));
+}
+
+void nextChar() {
+	c = getc(inputFile);
 }
